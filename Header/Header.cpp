3198ab410da0101c1909c5898f0250363a19f2ba
Ved  Lomber <vedlomber@gmail.com>
2023-03-26 18:57:28 +1100

    push

---
+++ Header/Header.cpp
@@ -0,0 +1,7 @@
+// Name: Header.cpp
+
+#include<iostream>
+using namespace std; //standard name space
+ int main() { //execution of the main program begins here.
+   cout<<"Welcome to C++ programming"; //display message
+ return 0;}
