19e23e9bac4b491776ecafae283a0fb253cd0632
Ved  Lomber <108036711+Civilisedfire453@users.noreply.github.com>
2023-03-28 20:54:50 +1100

    Delete Header.cpp
    duplicate from main

--- Header/Header.cpp
+++
@@ -1,7 +0,0 @@
-// Name: Header.cpp
-
-#include<iostream>
-using namespace std; //standard name space
- int main() { //execution of the main program begins here.
-   cout<<"Welcome to C++ programming"; //display message
- return 0;} //end of the program
