2b637e9560c6ba33a945f08bcb0dea3fa11749d9
Ved  Lomber <vedlomber@gmail.com>
2023-03-31 13:10:43 +1100

    made edits to the folders by combining them into 1

--- Cpp Projects/HelloWorld.cpp
+++ Cpp Projects/HelloWorld.cpp
@@ -1,8 +1,8 @@
-#include<iostream>
-using namespace std;
+#include<iostream> // this is the header file
+using namespace std; // this is the namespace
 
-int main()
+int main() // this is the main function
 {
-    cout<<"Hello World"<<endl;
-    return 0;
-}
+    cout<<"Hello World"<<endl; // this is the output
+    return 0; // this is the return statement
+} // this is the end of the program
