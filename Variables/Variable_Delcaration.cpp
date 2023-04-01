a271d2b2f0abc9d14cbf28e14ae7b9ea8f1ce644
Ved  Lomber <vedlomber@gmail.com>
2023-04-01 20:34:56 +1100

    commit  Created a new folder and sapmle code for variables.

---
+++ Variables/Variable_Delcaration.cpp
@@ -0,0 +1,19 @@
+#include<iostream> // default header file
+using namespace std; //standard namespace
+
+int main() // main function
+{
+    // variable declaration
+    int a = 10;
+    char B = 'B';
+    float c = 10.5; 
+    bool d = true;
+
+    // printing the values of variables  
+    cout << "a = " << a << endl;    
+    cout << "B = " << B << endl;
+    cout << "c = " << c << endl;
+    cout << "d = " << d << endl;
+
+    return 0; // return 0 to operating system
+} // end of main function
