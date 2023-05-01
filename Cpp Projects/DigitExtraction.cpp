506c36a68e381249e8bba74cddbf5ea983e0a0d0
Ved  Lomber <108036711+Civilisedfire453@users.noreply.github.com>
2023-05-01 10:45:05 +1000

    test

---
+++ Cpp Projects/DigitExtraction.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{   
+    // Declaration
+    int num,first,second,last = 0;
+
+    // Input
+    cout<<"Enter a number: ";
+    cin>>num;
+
+    // Logic
+    first = num /100;
+    second = (num / 10) % 10;
+    // another way to get the  second = (num%100) /10;
+    last = num % 10;
+    
+    // Output
+    cout<<"First digit: "<<first<<endl;
+    cout<<"Second digit: "<<second<<endl;
+    cout<<"Last digit: "<<last<<endl;    
+    return 0;
+} // End of main()
