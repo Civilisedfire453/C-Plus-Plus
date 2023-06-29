a12952936832824337bd336be3b20c35b8732802
Ved  Lomber <vedlomber@gmail.com>
2023-06-29 14:02:56 +1000

    Added Swith case and Character checker program

---
+++ Conditions/Switch_Case.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+
+// main function
+int main(){
+int day = 0;
+
+// ask user for input and store in variable
+std::cout<<"Please enter a number between 1 and 7: ";
+std::cin>>day;
+
+// switch statement
+switch(day){
+    // case 1 to 7
+    case 1:
+        std::cout<<"Monday";
+        break;
+    case 2:
+        std::cout<<"Tuesday";
+        break;
+    case 3:
+        std::cout<<"Wednesday";
+        break;
+    case 4:
+        std::cout<<"Thursday";
+        break;
+    case 5:
+        std::cout<<"Friday";
+        break;
+    case 6:
+        std::cout<<"Saturday";
+        break;
+    case 7:
+        std::cout<<"Sunday";
+        break;
+    default:
+        std::cout<<"Invalid input! Please enter a number between 1 and 7.";
+        break;
+    }
+    // end of switch statement
+
+return 0; 
+// end of main function
+}
---
+++ Cpp Projects/Character_Checker.cpp
@@ -0,0 +1,24 @@
+//Write a program to check whether a entered character is lowercase ( a to z ) or uppercase ( A to Z ).
+
+#include <iostream>
+using namespace std;
+
+int main() {
+  char ch;
+  cout << "Enter a character: ";
+  cin >> ch;
+
+  // Check if the character is lowercase
+  if (islower(ch)) {
+    cout << "The character " << ch << " is lowercase." << endl;
+  } else {
+    // Check if the character is uppercase
+    if (isupper(ch)) {
+      cout << "The character " << ch << " is uppercase." << endl;
+    } else {
+      cout << "The character " << ch << " is not a letter." << endl;
+    }
+  }
+
+  return 0;
+}
