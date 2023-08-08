59b7a0c1f3af864cd62a8758d482c3e0e4b2f3d5
Ved  Lomber <vedlomber@gmail.com>
2023-08-09 09:47:18 +1000

    Added functions intro

---
+++ Functions/Sample_Function.cpp
@@ -0,0 +1,21 @@
+// Include the input/output stream library
+#include <iostream>
+
+// #include <string>
+using namespace std;
+
+// Function declaration
+void Print() {
+    cout << "Hello World!!!" << endl;
+}
+
+// Main function
+int main() {
+
+    // Call the function
+    Print();
+
+    // Return 0 to indicate normal termination
+    return 0;
+}
+
