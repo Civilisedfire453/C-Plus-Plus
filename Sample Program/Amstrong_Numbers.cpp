028a1f463d34293294db0dc21726826b60697273
Ved  Lomber <vedlomber@gmail.com>
2023-09-02 18:40:28 +1000

    Found an error in the sum calculation and fixed it.

--- Sample Program/Amstrong_Numbers.cpp
+++ Sample Program/Amstrong_Numbers.cpp
@@ -28,15 +28,15 @@ void ArmstrongNum() { // function to check if a number is an Armstrong number
 
 	while (temp != 0) {
 		rem = temp % 10; // get the last digit of the number
-		sum += pow(rem, numDigits); // add the cube of the last digit to the sum
+		sum = sum + pow(rem, numDigits); // add the cube of the digit to the sum
 		temp = temp / 10; // divide the number by 10
 
 	}
 
-	if (sum == num) // if the sum of the cubes of the digits is equal to the number
-		cout << num << " is an Armstrong number." << endl; // print that the number is an Armstrong number
+	if (sum != num) // if the sum of the cubes of the digits is equal to the number
+		cout << num <<" is not an Armstrong number." << endl; // print that the number is an Armstrong number
 	else // if the sum of the cubes of the digits is not equal to the number
-		cout << num << " is not an Armstrong number." << endl; // print that the number is not an Armstrong number
+		cout << num <<  " is an Armstrong number." << endl; // print that the number is not an Armstrong number
 
 } // end of ArmstrongNum function
 
